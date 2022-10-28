fbc077bbe44d4379809b145f22c6840db9d03eb9
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2022-10-28 18:18:05 -0400

    added psuedocode to script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// ask user how many
+
+// get input from user. Can be either Rock, Paper, or Scissors = playerSelecetion
+
+// have computer randomly choose Rock, paper, or scissors
+
+// compare users input with computers input
+
+
+// if playerSelection === computerSelection
+    // its a draw
+
+
+//else if playerSelection === rock && computerSelection = paper
+    //or playerSelection === paper && computerSelection ===scissors
+    //or playerSelection === scissors && computerSelection === rock
+    // player loses
+
+// else player wins
+
+//if player wins, add tally to players score
+//if computer wins, add tally to computers score
+
+//
